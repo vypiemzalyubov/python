a179da98e4db3c0591f91de16cee854f7bd0537d
vypiemzalyubov <dima.rza90@mail.ru>
2023-03-10 21:01:41 +0300

    add selenium

---
+++ Selenium/Stepik/Test_automation_using_Selenium_and_Python/module_1/1_6_1.py
@@ -0,0 +1,30 @@
+# Задание: поиск элементов с помощью Selenium
+# Вам нужно открыть страницу по ссылке и заполнить форму на этой странице с помощью Selenium. 
+# Если всё сделано правильно, то вы увидите окно с проверочным кодом. Это число вам нужно ввести в качестве ответа в этой задаче.
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
