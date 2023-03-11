5f676834fedf377d7387011dbb66899cd67b1868
vypiemzalyubov <dima.rza90@mail.ru>
2023-03-11 08:52:08 +0300

    Add Codewars

---
+++ Codewars/8 kyu/you_can't_code_under_pressure_#1.py
@@ -0,0 +1,14 @@
+# 8 kyu
+# You Can't Code Under Pressure #1
+# 
+# Code as fast as you can! You need to double the integer and return it.
+
+def double_integer(i):
+    return i * 2
+
+
+
+# Best Practices
+
+def doubleInteger(i):
+    return i + i
