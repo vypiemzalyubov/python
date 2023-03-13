c5d5cdbf9a969517c091d7afdc1b4ee0a1a9b5f9
vypiemzalyubov <dima.rza90@mail.ru>
2023-03-13 20:16:40 +0300

    add task 8 kyu

---
+++ Codewars/8 kyu/convert_a_boolean_to_a_string.py
@@ -0,0 +1,16 @@
+# 8 kyu
+# Convert a Boolean to a String
+# 
+# Implement a function which convert the given boolean value into its string representation.
+# Note: Only valid inputs will be given.
+
+def boolean_to_string(b):
+    return str(True) if b == True else str(False)
+
+# Best Practices
+
+def boolean_to_string(b):
+    return str(b)
+
+def boolean_to_string(b):
+    return 'True' if b else 'False'
