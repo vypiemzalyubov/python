466d10cef06f5b53ac178ec53750236bec32322b
vypiemzalyubov <dima.rza90@mail.ru>
2023-03-13 21:07:48 +0300

    add 8 kyu task

---
+++ Codewars/8 kyu/basic_variable_assignment.py
@@ -0,0 +1,11 @@
+# 8 kyu
+# Basic variable assignment
+# 
+# This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+# a == "code"
+# b == "wa.rs"
+# name == a + b
+
+a = "code"
+b = "wa.rs"
+name = a + b
