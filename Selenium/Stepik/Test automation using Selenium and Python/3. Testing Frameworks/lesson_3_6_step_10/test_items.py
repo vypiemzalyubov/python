015ddff5ad96d0353938a6acaec1aa0cf2beee4a
vypiemzalyubov <dima.rza90@mail.ru>
2023-03-23 19:44:20 +0300

    Add lesson 3.2 step 10

--- Selenium/Stepik/Test automation using Selenium and Python/3. Testing Frameworks/lesson_3_6_step_10/test_items.py
+++ Selenium/Stepik/Test automation using Selenium and Python/3. Testing Frameworks/lesson_3_6_step_10/test_items.py
@@ -3,7 +3,7 @@ import time
 
 
 def test_basket_button(browser):
-    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
     message = browser.find_element(By.XPATH, "//*[contains(@class, 'btn-add-to-basket')]")
