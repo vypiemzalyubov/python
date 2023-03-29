43617230fb1eac10e92d90f3fc94980b1609c026
vypiemzalyubov <dima.rza90@mail.ru>
2023-03-29 20:36:40 +0300

    Preparing for first test

--- Selenium/Stepik/Test automation using Selenium and Python/4. Applying the Page Object Model Pattern/test_main_page.py
+++ Selenium/Stepik/Test automation using Selenium and Python/4. Applying the Page Object Model Pattern/test_main_page.py
@@ -1,11 +1,9 @@
+from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link) 
-    go_to_login_page(browser)   
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()    
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page    
