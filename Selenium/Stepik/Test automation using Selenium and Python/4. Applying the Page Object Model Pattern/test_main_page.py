e4ed5a2be46f6414434bc7eaaa8b3a22461d4fec
vypiemzalyubov <dima.rza90@mail.ru>
2023-03-29 20:19:53 +0300

    Add pages folder

--- Selenium/Stepik/Test automation using Selenium and Python/4. Applying the Page Object Model Pattern/test_main_page.py
+++ Selenium/Stepik/Test automation using Selenium and Python/4. Applying the Page Object Model Pattern/test_main_page.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    browser.get(link) 
+    go_to_login_page(browser)   
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()    
