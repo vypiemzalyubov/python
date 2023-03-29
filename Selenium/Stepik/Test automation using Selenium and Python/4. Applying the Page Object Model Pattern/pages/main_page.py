c38c3c0dab37ca0bdf84829852b83eebb11f4df6
vypiemzalyubov <dima.rza90@mail.ru>
2023-03-29 21:00:59 +0300

    Add exception checking

--- Selenium/Stepik/Test automation using Selenium and Python/4. Applying the Page Object Model Pattern/pages/main_page.py
+++ Selenium/Stepik/Test automation using Selenium and Python/4. Applying the Page Object Model Pattern/pages/main_page.py
@@ -3,6 +3,11 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
+
     def go_to_login_page(self):
         login_link = self.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+            
--- Selenium/Stepik/Test automation using Selenium and Python/4. Applying the Page Object Model Pattern/test_main_page.py
+++ Selenium/Stepik/Test automation using Selenium and Python/4. Applying the Page Object Model Pattern/test_main_page.py
@@ -6,4 +6,10 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page    
+    page.go_to_login_page
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()        
