0e1fa54a6116e9fe00e378149074ec94862ae26e
vypiemzalyubov <dima.rza90@mail.ru>
2023-04-01 13:02:20 +0300

    Add 7 kyu new task

---
+++ Codewars/7 kyu/valid_parentheses.py
@@ -0,0 +1,38 @@
+# 7 kyu
+# Valid Parentheses
+# 
+# Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
+# The function should return true if the string is valid, and false if it's invalid.
+# 
+# Examples
+# "()"              =>  true
+# ")(()))"          =>  false
+# "("               =>  false
+# "(())((()())())"  =>  true
+# 
+# Constraints
+# 0 <= length of input <= 100
+# 
+# All inputs will be strings, consisting only of characters ( and ).
+# Empty strings are considered balanced (and therefore valid), and will be tested.
+# For languages with mutable strings, the inputs should not be mutated.
+
+def valid_parentheses(paren_str):
+    while '()' in paren_str:
+        paren_str = paren_str.replace('()', '')
+    return True if paren_str == '' else False  
+
+
+
+# Best Practices
+
+def valid_parentheses(array):
+    count = 0
+    for item in array:
+        if item == '(':
+            count += 1
+        elif count:
+            count -= 1
+        else:
+            return False
+    return count == 0
