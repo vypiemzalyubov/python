62b0d9ffba57a5b5c57a538adf0d20a44a203e12
vypiemzalyubov <dima.rza90@mail.ru>
2023-04-02 21:14:40 +0300

    Add 8 kyu task

---
+++ Codewars/8 kyu/beginner_reduce_but_grow.py
@@ -0,0 +1,36 @@
+# 8 kyu
+# Beginner - Reduce but Grow
+# 
+# Given a non-empty array of integers, return the result of multiplying the values together in order. 
+# Example:
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+def grow(arr):
+    res = 1
+    for i in arr:
+        res *= i
+    return res 
+
+
+
+# Best Practices
+
+from functools import reduce
+
+def grow(arr):
+    return reduce(lambda x, y: x * y, arr)
+
+
+
+import math
+
+def grow(arr):
+    return math.prod(arr)
+
+
+
+from functools import reduce
+from operator import mul
+
+def grow(arr):
+    return reduce(mul, arr, 1)
