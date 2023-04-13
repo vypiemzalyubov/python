500067e23cf7280f221ac8bf49a09b66408467b4
vypiemzalyubov <dima.rza90@mail.ru>
2023-04-13 20:06:56 +0300

    Add 7 kyu task

---
+++ Codewars/7 kyu/person_class_bug.py
@@ -0,0 +1,42 @@
+# 7 kyu
+# Person Class Bug
+# 
+# The following code was thought to be working properly, however when the code tries to access the age of the person instance it fails.
+# 
+# person = Person('Yukihiro', 'Matsumoto', 47)
+# print(person.full_name)
+# print(person.age)
+# 
+# For this exercise you need to fix the code so that it works correctly.
+# Note: the order of the person's full name is first name and last name.
+
+class Person():
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.full_name = f'{self.first_name} {self.last_name}'
+
+
+
+# Best Practices
+
+class Person():
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+
+
+class Person():
+  
+    def __init__(self, first_name, last_name, age):
+        self.full_name = first_name + " " + last_name
+        self.age = age
