0bc7f9038283a6add90471b4a0b37215094ae4bd
vypiemzalyubov <dima.rza90@mail.ru>
2023-04-15 08:04:08 +0300

    Update Automation Craft

--- Selenium/Automation Craft/tests/elements_test.py
+++ Selenium/Automation Craft/tests/elements_test.py
@@ -30,7 +30,7 @@ class TestElements:
 
     class TestRadioButton:
 
-        def test_radio_button(seld, driver):
+        def test_radio_button(self, driver):
             radio_button_page = RadioButtonPage(driver, "https://demoqa.com/radio-button")
             radio_button_page.open()
             radio_button_page.click_on_the_radio_button("yes")
