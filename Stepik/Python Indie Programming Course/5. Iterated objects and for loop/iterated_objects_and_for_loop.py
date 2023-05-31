ce6cf987a0af045cfb73c564947b885d22af6c51
vypiemzalyubov <dima.rza90@mail.ru>
2023-05-31 20:51:37 +0300

    Update nested loop task

--- Stepik/Python Indie Programming Course/5. Iterated objects and for loop/iterated_objects_and_for_loop.py
+++ Stepik/Python Indie Programming Course/5. Iterated objects and for loop/iterated_objects_and_for_loop.py
@@ -337,7 +337,7 @@ print(count)
 
 n, count = int(input()), 0
 l = list(map(int, input().split()))
-for i in range(n):
+for i in range(n - 1):
     for j in range(n - i - 1):
         if l[j] > l[j + 1]:
             l[j], l[j + 1] = l[j + 1], l[j]
