3e99cb2b63938dfe2cdce938c7f054811c6de35c
dnikolaev90 <dnikolaev@git.xtools.tv>
2023-06-13 09:38:50 +0300

    Update dict task

--- Stepik/Python Indie Programming Course/6. None, dictionaries, sets and tuples/none_dictionaries_sets_and_tuples.py
+++ Stepik/Python Indie Programming Course/6. None, dictionaries, sets and tuples/none_dictionaries_sets_and_tuples.py
@@ -327,3 +327,13 @@ account = {
   "swift_bic": "BCYPGB2LHGB"
 }
 print(keys := list(account))
+
+# 312. Перед вами два словаря d1 и d2. Ваша задача выполнить слияние этих словарей в переменную capitals и затем вывести ее на экран.
+
+d1 = {'India': 'Delhi',
+      'Canada': 'Ottawa',
+      'United States': 'Washington D. C.'}
+
+d2 = {'France': 'Paris',
+      'Malaysia': 'Kuala Lumpur'}
+print(capitals := d1 | d2)
