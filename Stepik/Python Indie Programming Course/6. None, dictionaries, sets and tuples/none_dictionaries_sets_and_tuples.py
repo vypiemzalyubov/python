1f0abe943dddafd31285c53e075e9a55dc216da1
dnikolaev90 <dnikolaev@git.xtools.tv>
2023-06-13 09:34:12 +0300

    Update dict task

--- Stepik/Python Indie Programming Course/6. None, dictionaries, sets and tuples/none_dictionaries_sets_and_tuples.py
+++ Stepik/Python Indie Programming Course/6. None, dictionaries, sets and tuples/none_dictionaries_sets_and_tuples.py
@@ -314,4 +314,16 @@ currencies = {
 }
 print(currencies[c] if (c := input()) in currencies else f'Нет данных по {c}')
 
-# 311. 
+# 311. Перед вами словарь account.Ваша задача сохранить в переменную keys список из всех ключей словаря account при помощи функции list.
+#      В качестве ответа выведите переменную keys.
+
+account = {
+  "id": 3136,
+  "uid": "1359acc6-f07a-4a2a-984e-3fb809982948",
+  "account_number": "0847799459",
+  "iban": "GB90XTND56373623909314",
+  "bank_name": "ABN AMRO HOARE GOVETT CORPORATE FINANCE LTD.",
+  "routing_number": "126602476",
+  "swift_bic": "BCYPGB2LHGB"
+}
+print(keys := list(account))
