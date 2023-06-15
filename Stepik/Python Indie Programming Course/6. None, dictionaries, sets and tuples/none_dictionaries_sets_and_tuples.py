3da6cc77ace9a653b0f5bd76e54c0907f8202ce2
dnikolaev90 <dnikolaev@git.xtools.tv>
2023-06-15 14:28:39 +0300

    Add dict generators task

--- Stepik/Python Indie Programming Course/6. None, dictionaries, sets and tuples/none_dictionaries_sets_and_tuples.py
+++ Stepik/Python Indie Programming Course/6. None, dictionaries, sets and tuples/none_dictionaries_sets_and_tuples.py
@@ -588,3 +588,34 @@ data = {
 }
 l = [*sorted(i["first_name"] for i in data["my_friends"]["people"])]
 print(*(name for name in l), sep='\n')
+
+# 324. Перед вами словарь user. Напишите программу для создания нового словаря, которая извлекает указанные ключи из приведенного ниже словаря.
+#      Сами значения ключей, которые нужно извлечь, поступают на вход программе в виде одной строки разделенные пробелом. 
+#      В качестве ответа выведите на экран полученный словарь.
+
+user = {
+    "id": 4170,
+    "uid": "5e941db5-9e0f-4f94-9fc5-734110c6be14",
+    "password": "SyUpfo1ljm",
+    "first_name": "Teresa",
+    "last_name": "Wehner",
+    "username": "teresa.wehner",
+    "email": "teresa.wehner@email.com",
+    "gender": "Non-binary",
+    "phone_number": "+674 424.561.2776",
+    "social_insurance_number": "637316241",
+    "date_of_birth": "1993-08-17",
+    "employment": {
+        "title": "Central Hospitality Liaison",
+        "key_skill": "Organisation"
+    },
+    "subscription": {
+        "plan": "Essential",
+        "status": "Idle",
+        "payment_method": "Debit card",
+        "term": "Annual"
+    }
+}
+print({i: user[i] for i in input().split()})
+
+# 325. 
