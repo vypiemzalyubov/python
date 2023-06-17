451e800f334279819c898eac20bdd567709a7e07
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-17 12:19:15 +0300

    Update json task

--- Stepik/Python Indie Programming Course/9. Working with files/working_with_files.py
+++ Stepik/Python Indie Programming Course/9. Working with files/working_with_files.py
@@ -5,6 +5,7 @@ import json
 
 d = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 
      'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
+
 json_data = json.dumps(d)
 print(json_data)
 
@@ -19,6 +20,7 @@ from pprint import pprint
 max_sum = 0
 max_f_name = ''
 max_l_name = ''
+
 with open('manager_sales.json') as file:
     data = json.load(file)
 
