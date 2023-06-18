addb11545aca420f0283ec9e97f94db60f46ad91
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-18 15:51:29 +0300

    Update module 2

--- Automation/Python REST API Automation/2. Simple API queries/json_parsing.py
+++ Automation/Python REST API Automation/2. Simple API queries/json_parsing.py
@@ -7,5 +7,5 @@ import json
 
 json_text = '''{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'''
 
-data = json.loads(json_text)
-print(data["messages"][1]["message"])
+obj = json.loads(json_text)
+print(obj["messages"][1]["message"])
