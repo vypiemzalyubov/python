948618ef9e68128ef355dac507942a5090259a43
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-18 19:25:19 +0300

    Update task

--- Automation/Python REST API Automation/main.py
+++ Automation/Python REST API Automation/main.py
@@ -1,25 +1,5 @@
 import requests
 
-
-response = requests.post('https://playground.learnqa.ru/api/get_301', allow_redirects=True)
-first_response = response.history[0]
-second_response = response
-
-print(first_response.url)
-print(second_response.url)
-
-
-
-
-# from json.decoder import JSONDecodeError
-# import requests
-
-
-# response = requests.get('https://playground.learnqa.ru/api/get_text')
-# print(response.text)
-
-# try:
-#     parsed_response = response.json()
-#     print(parsed_response)
-# except JSONDecodeError:
-#     print("Response is not JSON format")
+payload = {'name': 'User'}
+response = requests.get('https://playground.learnqa.ru/api/hello', params=payload)
+print(response.text)
