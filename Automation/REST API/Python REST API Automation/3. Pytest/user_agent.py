8767c5c87785a534304153bb689bdfc0f3868223
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-19 22:23:17 +0300

    Update user_agent task

--- Automation/REST API/Python REST API Automation/3. Pytest/user_agent.py
+++ Automation/REST API/Python REST API Automation/3. Pytest/user_agent.py
@@ -18,3 +18,52 @@
 # )
 # На самом деле метод не всегда работает правильно. Ответом к задаче должен быть список из тех User Agent, 
 # которые вернули неправильным хотя бы один параметр, с указанием того, какой именно параметр неправильный.
+
+import pytest
+import requests
+
+class TestUserAgent:
+
+    user_agents = [
+        ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"),
+        # 'platform': 'Mobile', 'browser': 'No', 'device': 'Android'
+        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1"),
+        # 'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'
+        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"),
+        # 'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'
+        ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0"),
+        # 'platform': 'Web', 'browser': 'Chrome', 'device': 'No'
+        ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
+        # 'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'
+    ]
+
+    @pytest.mark.parametrize("user_agent", user_agents)
+    def test_check_user_agent(self, user_agent):
+
+        url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
+        headers={"User-Agent": user_agent}
+
+        if user_agent == "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30":
+            response1 = requests.get(url, headers=headers)
+            assert response1.status_code == 200, "Wrong response status code"
+            assert response1.json()["platform"] == 'Mobile' and response1.json()["browser"] == 'No' and response1.json()["device"] == 'Android', f"Wrong answer to request with user-agent {response1.request.headers}"
+
+        if user_agent == "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1":
+            response2 = requests.get(url, headers=headers)
+            assert response2.status_code == 200, "Wrong response status code"
+            assert response2.json()["platform"] == 'Mobile' and response2.json()["browser"] == 'Chrome' and response2.json()["device"] == 'iOS', f"Wrong answer to request with user-agent {response2.request.headers}"
+
+        if user_agent == "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)":
+            response2 = requests.get(url, headers=headers)
+            assert response2.status_code == 200, "Wrong response status code"
+            assert response2.json()["platform"] == 'Googlebot' and response2.json()["browser"] == 'Unknown' and response2.json()["device"] == 'Unknown', f"Wrong answer to request with user-agent {response2.request.headers}"            
+
+        if user_agent == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0":
+            response2 = requests.get(url, headers=headers)
+            assert response2.status_code == 200, "Wrong response status code"
+            assert response2.json()["platform"] == 'Web' and response2.json()["browser"] == 'Chrome' and response2.json()["device"] == 'No', f"Wrong answer to request with user-agent {response2.request.headers}"
+
+        if user_agent == "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1":
+            response2 = requests.get(url, headers=headers)
+            assert response2.status_code == 200, "Wrong response status code"
+            assert response2.json()["platform"] == 'Mobile' and response2.json()["browser"] == 'No' and response2.json()["device"] == 'iPhone', f"Wrong answer to request with user-agent {response2.request.headers}"            
