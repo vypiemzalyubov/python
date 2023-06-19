fa8ac15d23588820a14ae4074ade6db51fea00f4
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-19 22:27:22 +0300

    Update user_agent task

--- Automation/REST API/Python REST API Automation/3. Pytest/user_agent.py
+++ Automation/REST API/Python REST API Automation/3. Pytest/user_agent.py
@@ -26,7 +26,7 @@ class TestUserAgent:
 
     user_agents = [
         ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"),
-        # 'platform': 'Mobile', 'browser': 'No', 'device': 'Android'
+        # Expected values: 'platform': 'Mobile', 'browser': 'No', 'device': 'Android'
         ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1"),
         # 'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'
         ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"),
