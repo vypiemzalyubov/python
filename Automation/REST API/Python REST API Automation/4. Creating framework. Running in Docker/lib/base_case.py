db9eb61a28b84b2a009504f2b56644a42dbdb0ab
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-20 20:55:36 +0300

    Add base_case.py

---
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/lib/base_case.py
@@ -0,0 +1,22 @@
+from requests import Response
+import json.decoder
+
+class BaseCase:
+
+    def get_cookie(self, response: Response, cookie_name):
+        assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
+        return response.cookies[cookie_name]
+    
+    def get_header(self, response: Response, header_name):
+        assert header_name in response.headers, f"Cannot find header with name {header_name} in the last response"
+        return response.headers[header_name]
+    
+    def get_json_value(self, response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecoderError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't key '{name}'"
+
+        return response_as_dict[name]
--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_auth_user.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_auth_user.py
@@ -20,10 +20,7 @@ class TestUserAuth(BaseCase):
 
         self.auth_sid = self.get_cookie(response1, "auth_sid")
         self.token = self.get_header(response1, "x-csrf-token")
-        
-        assert "user_id" in response1.json(), "There is no user id in the response"
-
-        self.user_id_from_auth_method = response1.json()["user_id"]        
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
     def test_auth_user(self):
 
