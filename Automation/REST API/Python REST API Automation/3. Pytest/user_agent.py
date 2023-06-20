62612af16a0a8aaf088883d62d759c05d7b48739
dnikolaev90 <dnikolaev@git.xtools.tv>
2023-06-20 16:27:14 +0300

    Update user_agent task

--- Automation/REST API/Python REST API Automation/3. Pytest/user_agent.py
+++ Automation/REST API/Python REST API Automation/3. Pytest/user_agent.py
@@ -11,74 +11,51 @@
 # Наша задача написать параметризированный тест. Этот тест должен брать из дата-провайдера User Agent и ожидаемые значения, GET-делать запрос с этим User Agent и убеждаться, 
 # что результат работы нашего метода правильный - т.е. в ответе ожидаемое значение всех трех полей.
 # Список User Agent и ожидаемых значений можно найти по этой ссылке: https://gist.github.com/KotovVitaliy/138894aa5b6fa442163561b5db6e2e26.
-# Пример того, как должен выглядеть запрос с указанным User Agent:
-# requests.get(
-#     "https://playground.learnqa.ru/ajax/api/user_agent_check",
-#     headers={"User-Agent": "Some value here"}
-# )
-# На самом деле метод не всегда работает правильно. Ответом к задаче должен быть список из тех User Agent, 
-# которые вернули неправильным хотя бы один параметр, с указанием того, какой именно параметр неправильный.
+# На самом деле метод не всегда работает правильно. 
+# Ответом к задаче должен быть User Agent, который вернул неправильным хотя бы один параметр, с указанием того, какой именно параметр неправильный.
 
 import pytest
 import requests
 
 class TestUserAgent:
 
-    user_agents = [
-        ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"),
-        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1"),
-        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"),            
-        ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0"),            
-        ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
+    data_provider = [
+        ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+         {"platform": "Mobile", 
+         "browser": "No", 
+         "device": "Android"}),
+        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1", 
+         {"platform": "Mobile", 
+         "browser": "Chrome", 
+         "device": "iOS"}),
+        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+         {"platform": "Googlebot", 
+         "browser": "Unknown", 
+         "device": "Unknown"}),            
+        ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+         {"platform": "Web", 
+         "browser": "Chrome", 
+         "device": "No"}),            
+        ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+         {"platform": "Mobile", 
+         "browser": "No", 
+         "device": "iPhone"})
     ]    
 
-    expected_results = [
-        {"platform": "Mobile", 
-         "browser": "No", 
-         "device": "Android"},
-        {"platform": "Mobile", 
-         "browser": "Chrome", 
-         "device": "iOS"},
-        {"platform": "Googlebot", 
-         "browser": "Unknown", 
-         "device": "Unknown"},            
-        {"platform": "Web", 
-         "browser": "Chrome", 
-        "device": "No"},            
-        {"platform": "Mobile", 
-         "browser": "No", 
-         "device": "iPhone"}
-    ]
+    @pytest.mark.parametrize("user_agent, expected_results", data_provider)
+    def test_check_user_agent(self, user_agent, expected_results):
 
-    @pytest.mark.parametrize("user_agent", user_agents)
-    @pytest.mark.parametrize("expected_result", expected_results)
-    def test_check_user_agent(self, user_agent, expected_result):
-        
         url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
         headers={"User-Agent": user_agent}
 
-        if user_agent == "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30":
-            response1 = requests.get(url, headers=headers)
-            print('AAAAAAAAAAAAAAAA', expected_result[0]["platform"])
-            assert response1.status_code == 200, "Wrong response status code"
-            assert response1.json()["platform"] == expected_result[0]["platform"] and response1.json()["browser"] == 'No' and response1.json()["device"] == 'Android', f"Wrong answer to request with user-agent {response1.request.headers}"
-
-        # if user_agent == "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1":
-        #     response2 = requests.get(url, headers=headers)
-        #     assert response2.status_code == 200, "Wrong response status code"
-        #     assert response2.json()["platform"] == 'Mobile' and response2.json()["browser"] == 'Chrome' and response2.json()["device"] == 'iOS', f"Wrong answer to request with user-agent {response2.request.headers}"
-
-        # if user_agent == "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)":
-        #     response3 = requests.get(url, headers=headers)
-        #     assert response3.status_code == 200, "Wrong response status code"
-        #     assert response3.json()["platform"] == 'Googlebot' and response3.json()["browser"] == 'Unknown' and response3.json()["device"] == 'Unknown', f"Wrong answer to request with user-agent {response3.request.headers}"            
-
-        # if user_agent == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0":
-        #     response4 = requests.get(url, headers=headers)
-        #     assert response4.status_code == 200, "Wrong response status code"
-        #     assert response4.json()["platform"] == 'Web' and response4.json()["browser"] == 'Chrome' and response4.json()["device"] == 'No', f"Wrong answer to request with user-agent {response4.request.headers}"
-
-        # if user_agent == "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1":
-        #     response5 = requests.get(url, headers=headers)
-        #     assert response5.status_code == 200, "Wrong response status code"
-        #     assert response5.json()["platform"] == 'Mobile' and response5.json()["browser"] == 'No' and response5.json()["device"] == 'iPhone', f"Wrong answer to request with user-agent {response5.request.headers}"            
+        response = requests.get(url, headers=headers)
+        
+        assert expected_results["platform"] == response.json()["platform"],\
+            f'Incorrect parameter "{response.json()["platform"]}" in the response to the request with the User-Agent: {response.request.headers}'
+        
+        assert expected_results["browser"] == response.json()["browser"],\
+            f'Incorrect parameter "{response.json()["browser"]}" in the response to the request with the User-Agent: {response.request.headers}'
+        
+        assert expected_results["device"] == response.json()["device"],\
+                f'Incorrect parameter "{response.json()["device"]}" in the response to the request with the User-Agent: {response.request.headers}'
+ 
--- Automation/REST API/Python REST API Automation/main.py
+++ Automation/REST API/Python REST API Automation/main.py
@@ -1,34 +1,6 @@
 import pytest
 
-class TestUserAgent:
-
-    user_agents = [{
-        ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"): 
-            {"platform": "Mobile", 
-             "browser": "No", 
-             "device": "Android"},
-        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1"):
-            {"platform": "Mobile", 
-             "browser": "Chrome", 
-             "device": "iOS"},
-        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"):
-            {"platform": "Googlebot", 
-             "browser": "Unknown", 
-             "device": "Unknown"},            
-        ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0"):
-            {"platform": "Web", 
-             "browser": "Chrome", 
-             "device": "No"},            
-        ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"):
-            {"platform": "Mobile", 
-             "browser": "No", 
-             "device": "iPhone"}
-    }]    
-
-    @pytest.mark.parametrize("user_agent", user_agents)
-    def test_test(self, user_agent):
-        pass
-    
-    for i in user_agents:
-        print(i)
-        print()
+class TestA:
+    @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+    def test_eval(self, test_input, expected):
+        assert eval(test_input) == expected
