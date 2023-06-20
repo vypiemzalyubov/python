ef4ff30cbb2f50dbe89208031bfd4120de727501
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-20 20:12:30 +0300

    Update test auth

--- Automation/REST API/Python REST API Automation/3. Pytest/test_auth_user.py
+++ Automation/REST API/Python REST API Automation/3. Pytest/test_auth_user.py
@@ -4,8 +4,13 @@ import requests
 
 class TestUserAuth:
 
-    def test_auth_user(self):
-        
+    exclude_params = [
+        ("no_cookie"),
+        ("no_token")
+    ]
+
+    def setup(self):
+
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
@@ -17,54 +22,36 @@ class TestUserAuth:
         assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response"
         assert "user_id" in response1.json(), "There is no user id in the response"
 
-        auth_sid = response1.cookies.get("auth_sid")
-        token = response1.headers.get("x-csrf-token")
-        user_id_from_auth_method = response1.json()["user_id"]
+        self.auth_sid = response1.cookies.get("auth_sid")
+        self.token = response1.headers.get("x-csrf-token")
+        self.user_id_from_auth_method = response1.json()["user_id"]        
+
+    def test_auth_user(self):
 
         response2 = requests.get(
             "https://playground.learnqa.ru/api/user/auth",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
+            headers={"x-csrf-token": self.token},
+            cookies={"auth_sid": self.auth_sid}
         )
 
         assert "user_id" in response2.json(), "There is no user id in the second response"
 
         user_id_from_check_method = response2.json()["user_id"]
 
-        assert user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal from user id from check method"
-
-
-    exclude_params = [
-        ("no_cookie"),
-        ("no_token")
-    ]
+        assert self.user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal from user id from check method"
 
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
         
-        data = {
-            'email': 'vinkotov@example.com',
-            'password': '1234'
-        }
-
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
-
-        assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
-        assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response"
-        assert "user_id" in response1.json(), "There is no user id in the response"
-
-        auth_sid = response1.cookies.get("auth_sid")
-        token = response1.headers.get("x-csrf-token")
-        
         if condition == "no_cookie":
             response2 = requests.get(
                 "https://playground.learnqa.ru/api/user/auth",
-            headers={"x-csrf-token": token}
+            headers={"x-csrf-token": self.token}
             )
         else:
             response2 = requests.get(
                 "https://playground.learnqa.ru/api/user/auth",
-                cookies={"auth_sid": auth_sid}
+                cookies={"auth_sid": self.auth_sid}
             )
 
         assert "user_id" in response2.json(), "There is no user id in the second response"
