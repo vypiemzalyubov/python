6ef5b51235c39010df4cc763e12b1636d45f6cbb
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-20 21:09:50 +0300

    Update tests

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/lib/assertions.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/lib/assertions.py
@@ -1,3 +1,14 @@
+from requests import Response
+import json
 
 class Assertions:
-    pass
+    
+    @staticmethod
+    def assert_json_value_by_name(response: Response, name, expected_value, error_message):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't key '{name}'"
+        assert response_as_dict[name] == expected_value, error_message
--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_auth_user.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_auth_user.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 from lib.base_case import BaseCase
+from lib.assertions import Assertions
 
 class TestUserAuth(BaseCase):
 
@@ -30,11 +31,12 @@ class TestUserAuth(BaseCase):
             cookies={"auth_sid": self.auth_sid}
         )
 
-        assert "user_id" in response2.json(), "There is no user id in the second response"
-
-        user_id_from_check_method = response2.json()["user_id"]
-
-        assert self.user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal from user id from check method"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_auth_method,
+            "User id from auth method is not equal to user id from check method"
+        )
 
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
@@ -50,8 +52,9 @@ class TestUserAuth(BaseCase):
                 cookies={"auth_sid": self.auth_sid}
             )
 
-        assert "user_id" in response2.json(), "There is no user id in the second response"
-
-        user_id_from_check_method = response2.json()["user_id"]
-
-        assert user_id_from_check_method == 0, f"User is authorized with condition {condition}"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is authorized with condition {condition}"
+        )
