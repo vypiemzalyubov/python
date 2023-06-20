69ebee2e71a989c5ce1024761f13ae7d2b06c453
dnikolaev90 <dnikolaev@git.xtools.tv>
2023-06-20 09:38:54 +0300

    Update

--- Automation/REST API/Python REST API Automation/3. Pytest/user_agent.py
+++ Automation/REST API/Python REST API Automation/3. Pytest/user_agent.py
@@ -26,44 +26,59 @@ class TestUserAgent:
 
     user_agents = [
         ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"),
-        # Expected values: 'platform': 'Mobile', 'browser': 'No', 'device': 'Android'
         ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1"),
-        # 'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'
-        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"),
-        # 'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'
-        ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0"),
-        # 'platform': 'Web', 'browser': 'Chrome', 'device': 'No'
+        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"),            
+        ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0"),            
         ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
-        # 'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'
+    ]    
+
+    expected_results = [
+        {"platform": "Mobile", 
+         "browser": "No", 
+         "device": "Android"},
+        {"platform": "Mobile", 
+         "browser": "Chrome", 
+         "device": "iOS"},
+        {"platform": "Googlebot", 
+         "browser": "Unknown", 
+         "device": "Unknown"},            
+        {"platform": "Web", 
+         "browser": "Chrome", 
+        "device": "No"},            
+        {"platform": "Mobile", 
+         "browser": "No", 
+         "device": "iPhone"}
     ]
 
     @pytest.mark.parametrize("user_agent", user_agents)
-    def test_check_user_agent(self, user_agent):
-
+    @pytest.mark.parametrize("expected_result", expected_results)
+    def test_check_user_agent(self, user_agent, expected_result):
+        
         url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
         headers={"User-Agent": user_agent}
 
         if user_agent == "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30":
             response1 = requests.get(url, headers=headers)
+            print('AAAAAAAAAAAAAAAA', expected_result[0]["platform"])
             assert response1.status_code == 200, "Wrong response status code"
-            assert response1.json()["platform"] == 'Mobile' and response1.json()["browser"] == 'No' and response1.json()["device"] == 'Android', f"Wrong answer to request with user-agent {response1.request.headers}"
+            assert response1.json()["platform"] == expected_result[0]["platform"] and response1.json()["browser"] == 'No' and response1.json()["device"] == 'Android', f"Wrong answer to request with user-agent {response1.request.headers}"
 
-        if user_agent == "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1":
-            response2 = requests.get(url, headers=headers)
-            assert response2.status_code == 200, "Wrong response status code"
-            assert response2.json()["platform"] == 'Mobile' and response2.json()["browser"] == 'Chrome' and response2.json()["device"] == 'iOS', f"Wrong answer to request with user-agent {response2.request.headers}"
+        # if user_agent == "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1":
+        #     response2 = requests.get(url, headers=headers)
+        #     assert response2.status_code == 200, "Wrong response status code"
+        #     assert response2.json()["platform"] == 'Mobile' and response2.json()["browser"] == 'Chrome' and response2.json()["device"] == 'iOS', f"Wrong answer to request with user-agent {response2.request.headers}"
 
-        if user_agent == "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)":
-            response3 = requests.get(url, headers=headers)
-            assert response3.status_code == 200, "Wrong response status code"
-            assert response3.json()["platform"] == 'Googlebot' and response3.json()["browser"] == 'Unknown' and response3.json()["device"] == 'Unknown', f"Wrong answer to request with user-agent {response3.request.headers}"            
+        # if user_agent == "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)":
+        #     response3 = requests.get(url, headers=headers)
+        #     assert response3.status_code == 200, "Wrong response status code"
+        #     assert response3.json()["platform"] == 'Googlebot' and response3.json()["browser"] == 'Unknown' and response3.json()["device"] == 'Unknown', f"Wrong answer to request with user-agent {response3.request.headers}"            
 
-        if user_agent == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0":
-            response4 = requests.get(url, headers=headers)
-            assert response4.status_code == 200, "Wrong response status code"
-            assert response4.json()["platform"] == 'Web' and response4.json()["browser"] == 'Chrome' and response4.json()["device"] == 'No', f"Wrong answer to request with user-agent {response4.request.headers}"
+        # if user_agent == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0":
+        #     response4 = requests.get(url, headers=headers)
+        #     assert response4.status_code == 200, "Wrong response status code"
+        #     assert response4.json()["platform"] == 'Web' and response4.json()["browser"] == 'Chrome' and response4.json()["device"] == 'No', f"Wrong answer to request with user-agent {response4.request.headers}"
 
-        if user_agent == "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1":
-            response5 = requests.get(url, headers=headers)
-            assert response5.status_code == 200, "Wrong response status code"
-            assert response5.json()["platform"] == 'Mobile' and response5.json()["browser"] == 'No' and response5.json()["device"] == 'iPhone', f"Wrong answer to request with user-agent {response5.request.headers}"            
+        # if user_agent == "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1":
+        #     response5 = requests.get(url, headers=headers)
+        #     assert response5.status_code == 200, "Wrong response status code"
+        #     assert response5.json()["platform"] == 'Mobile' and response5.json()["browser"] == 'No' and response5.json()["device"] == 'iPhone', f"Wrong answer to request with user-agent {response5.request.headers}"            
