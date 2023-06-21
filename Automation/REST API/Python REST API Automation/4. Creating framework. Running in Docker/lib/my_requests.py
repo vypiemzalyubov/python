2e72dfcecc2ce5b83a791af3b4308bea9c3e6171
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-21 21:40:40 +0300

    Add my_requestst.py

---
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/lib/my_requests.py
@@ -0,0 +1,44 @@
+import requests
+
+
+class MyRequests():
+
+    @staticmethod
+    def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        return MyRequests._send(url, data, headers, cookies, "POST")
+
+    @staticmethod
+    def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        return MyRequests._send(url, data, headers, cookies, "GET")
+    
+    @staticmethod
+    def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        return MyRequests._send(url, data, headers, cookies, "PUT")
+
+    @staticmethod
+    def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        return MyRequests._send(url, data, headers, cookies, "DELETE")        
+
+    @staticmethod
+    def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
+        
+        url = f"https://playground.learnqa.ru/api{url}"
+
+        if headers is None:
+            headers = {}
+
+        if cookies is None:
+            cookies = {}
+            
+        if method == "GET":
+            response = requests.get(url, params=data, headers=headers, cookies=cookies)
+        elif method == "POST":
+            response = requests.post(url, data=data, headers=headers, cookies=cookies)
+        elif method == "PUT":
+            response = requests.put(url, data=data, headers=headers, cookies=cookies)
+        elif method == "DELETE":
+            response = requests.delete(url, data=data, headers=headers, cookies=cookies)
+        else:
+            raise Exception(f"Bad HTTP method '{method}' was received")
+        
+        return response
