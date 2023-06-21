3dc0ee1c16be8e86aecbce73d83db2109092732c
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-21 21:46:39 +0300

    Update test user edit

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_edit.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_edit.py
@@ -1,6 +1,6 @@
-import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestUserEdit(BaseCase):
@@ -9,7 +9,7 @@ class TestUserEdit(BaseCase):
         # REGISTER
         register_data = self.prepare_registration_data()
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+        response1 = MyRequests.post("/user/", data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -25,7 +25,7 @@ class TestUserEdit(BaseCase):
             "password": password
         }
 
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        response2 = MyRequests.post("/user/login", data=login_data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
@@ -33,7 +33,7 @@ class TestUserEdit(BaseCase):
         # EDIT
         new_name = "Changed name"
 
-        response3 = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}", 
+        response3 = MyRequests.put(f"/user/{user_id}", 
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid},
                                  data={"firstName": new_name}
@@ -42,7 +42,7 @@ class TestUserEdit(BaseCase):
         Assertions.assert_code_status(response3, 200)
 
         # GET
-        response4 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}", 
+        response4 = MyRequests.get(f"/user/{user_id}", 
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid}
                                  )
