c41f78c48b874f05657eabc213e29b5769c1e06c
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-21 19:59:49 +0300

    Add test get user

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/lib/assertions.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/lib/assertions.py
@@ -15,7 +15,6 @@ class Assertions:
             response_as_dict = response.json()
         except json.JSONDecodeError:
             assert False, f"Response is not JSON format. Response text is '{response.text}'"
-
         assert name in response_as_dict, f"Response JSON doesn't key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
 
@@ -25,5 +24,21 @@ class Assertions:
             response_as_dict = response.json()
         except json.JSONDecodeError:
             assert False, f"Response is not JSON format. Response text is '{response.text}'"
-
         assert name in response_as_dict, f"Response JSON doesn't key '{name}'"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't key '{name}'"        
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it's present"        
