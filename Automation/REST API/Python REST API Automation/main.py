30eef06e5713d9d648d948a4c7e8dd8ad7a2c70c
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-21 23:20:16 +0300

    Add Dockerfile

--- Automation/REST API/Python REST API Automation/main.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-class TestA:
-    @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
-    def test_eval(self, test_input, expected):
-        assert eval(test_input) == expected
