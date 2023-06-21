8b77c7972e167fee93df465dd46807dab79785e6
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-21 22:55:15 +0300

    Update my requests

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/lib/my_requests.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/lib/my_requests.py
@@ -1,6 +1,7 @@
 import requests
 import allure
 from lib.logger import Logger
+from environment import ENV_OBJECT
 
 
 class MyRequests():
@@ -28,7 +29,7 @@ class MyRequests():
     @staticmethod
     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
         
-        url = f"https://playground.learnqa.ru/api{url}"
+        url = f"{ENV_OBJECT.get_base_url}{url}"
 
         if headers is None:
             headers = {}
