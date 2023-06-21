0f1a4a1c613a10731f1e9044691ec4b892baf219
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-21 21:48:09 +0300

    Update test user get

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_get.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_get.py
@@ -1,13 +1,13 @@
-import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestUserGet(BaseCase):
 
     def test_get_user_details_not_auth(self):
 
-        response = requests.get("https://playground.learnqa.ru/api/user/2")
+        response = MyRequests.get("/user/2")
         
         Assertions.assert_json_has_key(response, "username")
         Assertions.assert_json_has_not_key(response, "email")
@@ -21,13 +21,13 @@ class TestUserGet(BaseCase):
             "password": "1234"
         }        
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
 
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
         user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
-        response2 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}", 
+        response2 = MyRequests.get(f"/user/{user_id_from_auth_method}", 
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid}
                                  )
