9c6c84a96dba1138d9623573381ccea326bebfdb
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-24 10:01:02 +0300

    Add functions task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -1,1 +1,7 @@
-# 355.
+# 355. Давайте начнем с легкого примера. Создадим функцию с именем keanu_reeves, которая выводит сообщение "YOU'RE BREATHTAKING".and
+#      Ваша задача написать только определение функции keanu_reeves.
+
+def keanu_reeves():
+    print("YOU'RE BREATHTAKING")
+
+# 356. 
