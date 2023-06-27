06fbde5d6dbe9abe5360ded5feda54bb2d0d56a2
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-27 20:49:22 +0300

    Update func task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -108,3 +108,20 @@ text = input()
 symbol = input()
 
 count_letter(text, symbol)
+
+# 366. Напишите функцию print_initials(name, surname, middlename), которая принимает три параметра:
+#      - name – имя человека;
+#      - surname – фамилия человека;
+#      - middlename– отчество человека;
+#      а затем выводит на печать фамилию и инициалы в определенном формате (первая буква фамилии должна стать заглавной, все остальные строчные; 
+#      в имени и отчестве остаются только по одной букве в верхнем регистре).
+#      Ваша задача дописать только тело функции print_initials.
+
+def print_initials(name, surname, middlename):
+    print(f'{surname.title()} {name.upper()[0]}.{middlename.upper()[0]}.')
+
+name = input()
+surname = input()
+middlename = input()
+
+print_initials(name, surname, middlename)
