2eb83e7e280270c3d5bed218086a03d78cc540da
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-27 20:23:20 +0300

    Update func task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -76,3 +76,11 @@ def count_letters(s):
         n += char.isupper()
         k += char.islower() 
     print(f'Количество заглавных символов: {n}\nКоличество строчных символов: {k}')
+
+# 363. Напишите функцию repeat_please_n_times, которая принимает один аргумент n - натуральное число. 
+#      Функция repeat_please_n_times должна n раз распечатать фразу "Just do it" в отдельной строчке.
+#      Ваша задача написать только определение функции repeat_please_n_times, вызывать ее не нужно.
+
+def repeat_please_n_times(n):
+    for i in range(n):
+        print('Just do it')
