a22ec6a006612afba02b884cb3479a31066435b0
vypiemzalyubov <dima.rza90@mail.ru>
2023-06-27 20:40:51 +0300

    Update func task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -95,3 +95,16 @@ a, b, c = map(int, input().split())
 
 is_between(a, b, c)
 
+# 365. Напишите функцию count_letter(text, letter), которая принимает два параметра:
+#      - text – текст, в котором нужно посчитать сколько раз появилась буква letter, учитывая регистр буквы;
+#      - letter – буква, количество которой мы должны посчитать в text.
+#      Функция count_letter должна выводить на экран найденное количество букв  letter в тексте text.
+#      Ваша задача дописать только тело функции count_letter.
+
+def count_letter(text, letter):
+    print(sum([i.count(symbol) for i in text]))
+    
+text = input()
+symbol = input()
+
+count_letter(text, symbol)
