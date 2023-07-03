df9cc807bac51d7ab06bc29af1a450c731a84f8c
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-03 21:26:12 +0300

    Update return task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -206,3 +206,21 @@ def first_unique_char(s):
 
 s = input()
 print(first_unique_char(s))
+
+# 374. Ваша задача написать функцию format_name_list, которая принимает список словарей, у каждого словаря в этом списке есть только ключ name.
+#      Функция format_name_list должна вернуть строку, в которой все имена из списка разделяются запятой кроме последних двух имен, они должны быть разделены союзом "и". 
+#      Если в списке нет ни одного имени, функция должна вернуть пустую строку. Ниже представлены примеры:
+#      format_name_list([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]) => 'Bart, Lisa и Maggie'
+#      format_name_list([{'name': 'Bart'}, {'name': 'Lisa'}]) => 'Bart и Lisa'
+#      format_name_list([{'name': 'Bart'}]) => 'Bart'
+#      format_name_list([]) => ''
+#      Ваша задача написать только определение функции format_name_list.
+
+def format_name_list(names: list):
+    tmp = [i['name'] for i in names]
+    if len(tmp) == 0:
+        return ''
+    elif len(tmp) == 1:
+        return tmp[0]
+    s = ', '.join(tmp[0:-1])
+    return f'{s} и {tmp[-1]}'
