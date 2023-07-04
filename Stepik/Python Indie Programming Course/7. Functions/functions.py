692b12df5590f3512f2180efec9ae9f804581a6d
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-04 20:31:10 +0300

    Update return task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -224,3 +224,17 @@ def format_name_list(names: list):
         return tmp[0]
     s = ', '.join(tmp[0:-1])
     return f'{s} и {tmp[-1]}'
+
+# 375. Ваша задача написать функцию get_domain_name, которая принимает строку url, извлекает из нее доменное имя и возвращает его в качестве строки.
+#      get_domain_name("http://google.com") => "google"
+#      get_domain_name("http://google.co.jp") => "google"
+#      get_domain_name("www.xakep.ru") => "xakep"
+#      get_domain_name("https://youtube.com") => "youtube"
+#      get_domain_name("https://www.asos.com") => "asos"
+#      get_domain_name("http://www.lenovo.com") => "lenovo"
+#      Строка url может начинаться с протоколов http://  https:// или с www. URL, начинающиеся с протоколов http://  https://, могут также содержать www.
+#      Ваша задача написать только определение функции get_domain_name.
+
+def get_domain_name(url):
+    s = url.replace('http://', '').replace('https://', '').replace('www.', '', 1)
+    return s.split('.')[0]
