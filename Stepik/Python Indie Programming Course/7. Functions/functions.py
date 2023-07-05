07389fe5b22b436ce7c3443dce86dac66d752797
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-05 20:05:31 +0300

    Add annotations task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -321,3 +321,19 @@ def add_binary(a, b):
     """Возвращает сумму чисел a и b в двоичном виде"""
     binary_sum = bin(a+b)[2:]
     return binary_sum
+
+# 379. Напишите функцию first_repeated_word , которая принимает строку, состоящую из нескольких слов, слова разделяются между собой пробелом. 
+#      Функция должна найти первое повторяющееся слово и вернуть его в качестве результата. Если передана строка, в которой все слова различны, функция first_repeated_word должна вернуть None.
+#      Регистр букв при сравнении нужно учитывать
+#      first_repeated_word("ab ca bc ab") => "ab"
+#      first_repeated_word("ab ca bc Ab cA aB bc") => "bc"
+#      first_repeated_word("ab ca bc ca ab bc") => "ca"
+#      first_repeated_word("ab ca bc") => None
+#      Для функции first_repeated_word нужно добавить док-строку "Находит первый дубль в строке" и не забудьте проаннотировать аргументы и возврат функции.
+#      Нужно написать только определение функции first_repeated_word.
+
+def first_repeated_word(s: str, st = set()) -> str|None:
+    """Находит первый дубль в строке"""
+    for i in s.split():
+        if i not in st: st.add(i)
+        else: return i
