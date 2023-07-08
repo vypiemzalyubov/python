7c1aa8cd7e8cc0f912b588636f71af3f35d0910b
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-08 09:54:48 +0300

    Add func args task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -420,3 +420,17 @@ def get_word_indices(strings: list[str]) -> dict:
         for j in v.lower().split():
             d[j] = d.get(j, []) + [i]
     return d
+
+# 384. Напишите функцию replace, которая принимает три параметра:
+#      - обязательный строковый параметр text - текст, в котором необходимо выполнить замены;
+#      - обязательный строковый параметр old - строка поиска для замены;
+#      - необязательный строковый параметр new - значение замены для найденного значения old. По умолчанию равен пустой строке.
+#      Функция replace должна возвращать новую строку, в которой все символы old были заменены на new. При замене регистр букв должен учитываться.
+#      replace('Нет', 'т') => 'Не'
+#      replace('Bella Ciao', 'a') => 'Bell Cio'
+#      replace('nobody; i myself farewell analysis', 'l', 'z') => 'nobody; i mysezf farewezz anazysis'
+#      replace('commend me to my kind lord meaning', 'M', 'w') => 'commend me to my kind lord meaning'
+#      Ваша задача дописать только тело функции replace.
+
+def replace(text: str, old: str, new: str = ''):
+    return text.replace(old, new)
