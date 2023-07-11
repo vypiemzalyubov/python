2ba818a29ed02f557131ccda354f686a0e036005
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-11 21:31:26 +0300

    Update recursion task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -663,3 +663,17 @@ def tribonacci(n: int) -> int:
     if n == 2:
         return 1
     return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+
+# 399. Описать рекурсивную функцию get_combin, которая принимает на вход два целых числа  и находит C(N,K) — число сочетаний из N элементов по K — с помощью рекуррентного соотношения.
+#      При этом гарантируется, что входные значения n и k будут удовлетворять следующим условиям: n > 0, 0 ≤ k ≤ n.
+#      Вот примеры вызовов:
+#      get_combin(5, 5) => 1
+#      get_combin(5, 2) => 10
+#      get_combin(3, 1) => 3
+#      get_combin(7, 0) => 1
+#      Ваша задача только написать определение функции get_combin.
+
+def get_combin(n: int, k:int) -> int:
+    if k == 0 or k == n:
+        return 1
+    return get_combin(n - 1, k) + get_combin(n - 1, k - 1)
