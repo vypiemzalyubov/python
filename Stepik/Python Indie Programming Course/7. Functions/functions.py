ec4cbf988b589fd15cc8514d00091adf6e2d21f3
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-12 20:35:53 +0300

    Update recursion task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -718,3 +718,62 @@ def flatten(l):
     if isinstance(l[0], list):
         return flatten(l[0]) + flatten(l[1:])
     return l[:1] + flatten(l[1:])
+
+# 403. Перед вами имеется вложенный словарь, уровень вложенности произвольный и заранее неизвестен. Ключами словаря на любом уровне могут быть только строки, значения - только числа. 
+#      Учитывая указанные выше условия, ваша задача состоит в том, чтобы преобразовать этот вложенный словарь в плоский (состоящий только из одного уровня), 
+#      где ключи формируются конкатенацией вложенных ключей, соединенных знаком _.
+#      Для этого необходимо определить рекурсивную функцию flatten_dict. Она должна принимать вложенный словарь и возвращать плоский.
+#      Ниже приведены несколько способов решения вышеуказанной задачи.
+#      nested = {'Germany': {'berlin': 7},
+#                'Europe': {'italy': {'Rome': 3}},
+#                 'USA': {'washington': 1, 'New York': 4}}
+#      flatten_dict(nested) => {'Germany_berlin': 7,
+#                               'Europe_italy_Rome': 3,
+#                                'USA_washington': 1,
+#                                'USA_New York': 4}
+#      nested = {'Q': {'w': {'E': {'r': {'T': {'y': 123}}}}}}
+#      flatten_dict(nested) => {'Q_w_E_r_T_y': 123}
+#      not_nested = {'a': 100, 'b': 200}
+#      flatten_dict(not_nested) => {'a': 100, 'b': 200}
+#      Ваша задача только написать определение функции flatten_dict.
+
+def flatten_dict(d: dict) -> dict:
+    res = {}
+    for k, v in d.items():
+        if isinstance(v, int):
+            res.update({k:v})
+        if isinstance(v, dict):
+            for key, value in flatten_dict(v).items():
+                res.update({k +'_' + key: value})
+    return res
+
+# 404. Есть несколько типов сортировки, которые используют рекурсию. Одна из них называется сортировка слиянием.
+#      Ваша задача реализовать этот алгоритм. Для этого нужно будет создать функцию merge_sort, которая будет принимать исходный список
+#      и возвращать новый отсортированный в порядке неубывания список. Также для реализации функции merge_sort вам понадобится реализовать функцию merge_two_list. 
+#      Функция merge_two_list должна принимать два отсортиванных по неубыванию списка, сливать их в один большой список также отсортированный по неубыванию 
+#      (задача Слияние списков ) и возвращать его.
+#      Ваша задача написать только определение функций merge_sort и merge_two_list, при этом нельзя пользоваться встроенными сортировками в Python.
+
+def merge_two_list(a, b):
+    l = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            l.append(a[i])
+            i += 1
+        else:
+            l.append(b[j])
+            j += 1
+    if i < len(a):
+        l += a[i:]
+    if j < len(b):
+        l += b[j:]
+    return l
+
+def merge_sort(s):
+    if len(s) == 1:
+        return s
+    middle = len(s) // 2
+    left = merge_sort(s[:middle])
+    right = merge_sort(s[middle:])
+    return merge_two_list(left, right)
