dac3c6c7608b00cc678b3f179e4e6195ed81b2ac
dnikolaev90 <dnikolaev@git.xtools.tv>
2023-07-12 08:01:21 +0300

    Update recursion task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -677,3 +677,20 @@ def get_combin(n: int, k:int) -> int:
     if k == 0 or k == n:
         return 1
     return get_combin(n - 1, k) + get_combin(n - 1, k - 1)
+
+# 400. Описать рекурсивную функцию ackermann, которая принимает на вход два целых числа  m и n находит значение, определенное следующим образом.
+#      Найденное значение функция ackermann должна вернуть в качестве результата.
+#      ackermann(3, 2) => 29
+#      ackermann(3, 0) => 5
+#      ackermann(1, 0) => 2
+#      ackermann(3, 5) => 253
+#      Ваша задача только написать определение функции ackermann.
+#      В тестовых примерах сперва поступает на вход значение аргумента m, затем аргумента n.
+
+def ackermann(m, n):
+    if m == 0:
+        return n + 1
+    if m > 0 and n == 0:
+        return ackermann(m - 1, 1)
+    if m > 0 and n > 0:
+        return ackermann(m - 1, ackermann(m, n - 1))
