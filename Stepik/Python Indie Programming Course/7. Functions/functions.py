fa65e38dbfafe1580c0656e2eb3ce4330b9cd0d7
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-12 21:11:33 +0300

    Update recursion task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -777,3 +777,17 @@ def merge_sort(s):
     left = merge_sort(s[:middle])
     right = merge_sort(s[middle:])
     return merge_two_list(left, right)
+
+# 405. Быстрая сортировка - еще один вид сортировки, который использует рекурсию.
+#      Ваша задача реализовать этот алгоритм. Для этого нужно будет создать функцию quick_sort, 
+#      которая будет принимать исходный список и возвращать новый отсортированный в порядке неубывания список.
+#      Необходимо написать только определение функций quick_sort, при этом нельзя пользоваться встроенными сортировками в Python.
+
+def quick_sort(s):
+    if len(s) <= 1:
+        return s
+    elem = s[0]
+    left = list(filter(lambda x: x < elem, s))
+    center = [i for i in s if i == elem]
+    right = list(filter(lambda x: x > elem, s))
+    return quick_sort(left) + center + quick_sort(right)
