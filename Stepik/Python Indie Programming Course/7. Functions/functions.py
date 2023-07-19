3540595fab6a36616be0fde00148a3748206321d
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-19 19:59:13 +0300

    Add decorators task

--- Stepik/Python Indie Programming Course/7. Functions/functions.py
+++ Stepik/Python Indie Programming Course/7. Functions/functions.py
@@ -1085,3 +1085,29 @@ def create_dict():
         d.setdefault(count, x)
         return d
     return inner
+
+# 428. Напишите декоратор text_decor, который оборачивает вызов декорированной функции фразами «Hello» и «Goodbye!»: фраза «Hello» печатается до вызова, фраза «Goodbye!» - после
+#      @text_decor
+#      def simple_func():
+#          print('I just simple python func')
+#      simple_func()
+#      # Вывод
+#      Hello
+#      I just simple python func
+#      Goodbye!
+#      @text_decor
+#      def multiply(num1, num2):
+#          print(num1 * num2)
+#      multiply(3, 5)
+#      # Вывод
+#      Hello
+#      15
+#      Goodbye!
+#      Ваша задача написать только определение функции декоратора text_decor.
+
+def text_decor(func):
+    def inner(*args, **kwargs):
+        print('Hello')
+        func(*args, **kwargs)
+        print('Goodbye!')
+    return inner
