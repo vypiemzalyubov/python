32128779bc188423571d556c38faae0617f65386
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-20 19:53:18 +0300

    Update working with files task

--- Stepik/Python Indie Programming Course/9. Working with files/working_with_files.py
+++ Stepik/Python Indie Programming Course/9. Working with files/working_with_files.py
@@ -5,9 +5,35 @@ def file_read(file_name: str) -> None:
     file = open(file_name, encoding='utf-8')
     print(file.read())
 
+# 440. Напишите функцию file_n_lines, которая печатает первые n-строка файла. Функция file_n_lines принимает на вход название файла и количество строк для прочтения.
+#      Не забывайте избавляться от символа переноса строки К примеру, если бы имелся файл hello.txt со следующим содержимым:
+#      h
+#      he
+#      hel
+#      hell
+#      hello
+#      То вызов file_n_lines(hello.txt, 3) должен распечатать следующее:
+#      h
+#      he
+#      hel
+#      Ваша задача написать только определение функции file_n_lines.
 
+def file_n_lines(file_name: str, n: int) -> None:
+    file = open(file_name, encoding='utf-8')
+    [print(file.readline().strip()) for _ in range(n)]
 
+# 441. Напишите функцию create_file_with_numbers, которая принимает на вход одно целое положительное число - n.
+#      Функция должна создать файл с название "range_<number>.txt" и наполнить его целыми числами от 1 до n включительно, причем каждое число записывается  в отдельной строке.
+#      Пример: функция create_file_with_numbers(5) должна создать файл range_5.txt с содержимым
+#      1
+#      2
+#      3
+#      4
+#      5
 
+def create_file_with_numbers(n: int) -> None:
+    file = open(f'range_{n}.txt', 'a')
+    [file.write(f'{i}\n') for i in range(1, n + 1)]
 
 
 
