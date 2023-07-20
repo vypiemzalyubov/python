576e980b5d0b9e1ac233ca07eaa84b08c67d0ada
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-20 20:56:34 +0300

    Add context manager task

--- Stepik/Python Indie Programming Course/9. Working with files/working_with_files.py
+++ Stepik/Python Indie Programming Course/9. Working with files/working_with_files.py
@@ -71,9 +71,13 @@ for i in file:
         sum += int(i)
 print(f'{count},{sum}') 
 
+# 444. Напишите функцию find_lines_len_more_6, которая принимает имя файла и находит количество строк, превышающее 6 символов. 
+#      Не забывайте исключать знак переноса на новую строку, стоящий в конце строки. Функция find_lines_len_more_6 должна возвращать найденное количество строк.
+#      Ваша задача написать только определение функции find_lines_len_more_6.
 
-
-
+def find_lines_len_more_6(file_name:str) -> int:
+    with open(file_name, 'r', encoding='utf-8') as f:
+        return sum(len(line) > 6 for line in f.read().splitlines())
 
 
 
