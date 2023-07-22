ba7438a07eaff73eb9fb3a26eae50c01f79e71d8
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-22 09:09:41 +0300

    Update map task

--- Stepik/Python Indie Programming Course/10. Advanced Python/advanced_python.py
+++ Stepik/Python Indie Programming Course/10. Advanced Python/advanced_python.py
@@ -163,3 +163,30 @@ print(increase_3 := [*map(lambda x: x * 3, numbers)])
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print([*map(lambda x: x ** 2, numbers)], [*map(lambda x: x ** 3, numbers)], sep='\n')
+
+# 463. Помните задачку про RGB? Сейчас вам предстоит выполнить обратное преобразование. Ваша задача создать функцию from_hex_to_rgb, которая принимает на вход строку
+#      - закодированный код цвета в формате RGB и возвращает кортеж из трех значений (оттенок_красного, оттенок_зеленого, оттенок_синего). 
+#      Вот посмотрите примеры:
+#      from_hex_to_rgb(#000000) => (0, 0, 0)
+#      from_hex_to_rgb(#FFFFFF) => (255, 255, 255)
+#      from_hex_to_rgb(#FF0000) => (255,0, 0)
+#      from_hex_to_rgb(#00FF00) => (0,255, 0)
+#      from_hex_to_rgb(#0000FF) => (0,0,255)
+#      from_hex_to_rgb(#FFFFFF) => (255,255,255)
+#      from_hex_to_rgb(#87CEEB) => (135,206,235)
+#      from_hex_to_rgb(#87CEFA) => (135,206,250)
+#      from_hex_to_rgb(#191970) => (25,25,112)
+#      Как только функция будет готова, ее необходимо применить ко всем элементам списка colors при помощи функции map. 
+#      Ниже уже имеется готовый список colors и цикл обхода результата функции map. Добавьте только название функции внутри вызова map, все остальное код сделает за вас.
+
+def from_hex_to_rgb(color: str) -> tuple:
+    color = color[1:3], color[3:5], color[5:7]
+    return tuple(map(lambda x: int(x, 16), color))
+
+colors = ['#B22222', '#DC143C', '#FF0000', '#FF6347', '#FF7F50', '#CD5C5C', '#F08080', '#E9967A',
+          '#FA8072', '#FFA07A', '#FF4500', '#FF8C00', '#FFA500', '#FFD700', '#B8860B', '#DAA520',
+          '#EEE8AA', '#BDB76B', '#F0E68C', '#808000', '#FFFF00', '#9ACD32', '#556B2F', '#6B8E23',
+          '#7CFC00', '#7FFF00', '#ADFF2F']
+
+for red, green, blue in map(from_hex_to_rgb, colors):
+    print(f"Red={red}, Green={green}, Blue={blue}")
