9345a649a1ac0f2c97dc5bb56137d94e621ca22d
vypiemzalyubov <dima.rza90@mail.ru>
2023-07-23 12:40:38 +0300

    Update isinstance task

--- Stepik/Python Indie Programming Course/10. Advanced Python/advanced_python.py
+++ Stepik/Python Indie Programming Course/10. Advanced Python/advanced_python.py
@@ -426,3 +426,16 @@ assert zip_with_function([["a", "b"], ["1", "2"]], combine_strings) == ['a1', 'b
 
 def count_strings(*args):
     return sum(1 for c in args if isinstance(c, str))
+
+# 475. Ваша задача написать функцию find_keys, которая принимает произвольное количество именованных аргументов. Функция должна отобрать только те имена параметров, 
+#      у которых значения являются списками или кортежами. Функция find_keys должна собрать все имена таких параметров в список, отсортировать их по алфавиту 
+#      вне зависимости от регистра букв и вернуть в качестве результата.
+#      Ниже представлены примеры:
+#      find_keys(t=[4, 5], W=[5, 3], A=(3, 2), a={2, 3}, b=[4]) => ['A', 'b', 't', 'W']
+#      find_keys(name='Bruce', surname='Wayne') => []
+#      find_keys(marks=[4, 5], name='ashle', surname='Brown', age=20, Also=(1, 2)) => ['Also', 'marks']
+#      Ваша задача написать только определение функции find_keys.
+
+def find_keys(**kwargs):
+    keys = [k for k, v in kwargs.items() if isinstance(v, list|tuple)]
+    return sorted(keys, key=lambda x: x.lower())
