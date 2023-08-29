a387b2eb481c499d1fca9191b45406cccd47b90c
vypiemzalyubov <dima.rza90@mail.ru>
2023-08-29 12:04:42 +0300

    Add 6 kyu task

---
+++ Codewars/6 kyu/replace_with_alphabet_position.py
@@ -0,0 +1,32 @@
+# 6 kyu
+# 
+# Replace With Alphabet Position
+# 
+# Welcome.
+# In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+# If anything in the text isn't a letter, ignore it and don't return it.
+# "a" = 1, "b" = 2, etc.
+# 
+# Example
+# alphabet_position("The sunset sets at twelve o' clock.")
+# Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+def alphabet_position(text):
+    res = ''
+    for char in text.lower():
+        if char.isalpha():
+            res += (str(ord(char) - 96)) + ' '
+    return res.rstrip()
+
+
+
+# Best Practices
+
+def alphabet_position(text):
+    return ' '.join(str(ord(c) - 96) for c in text.lower() if c.isalpha())
+
+
+from string import ascii_lowercase
+
+def alphabet_position(text):
+    return ' '.join(str(ascii_lowercase.index(n.lower()) + 1) for n in text if n.isalpha())
