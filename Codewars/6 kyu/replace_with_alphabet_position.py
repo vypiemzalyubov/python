182820b98d43644d2aba964bafcb18d4d97e9305
vypiemzalyubov <dima.rza90@mail.ru>
2023-08-29 12:05:34 +0300

    Update 6 kyu task

--- Codewars/6 kyu/replace_with_alphabet_position.py
+++ Codewars/6 kyu/replace_with_alphabet_position.py
@@ -26,7 +26,43 @@ def alphabet_position(text):
     return ' '.join(str(ord(c) - 96) for c in text.lower() if c.isalpha())
 
 
+
 from string import ascii_lowercase
 
 def alphabet_position(text):
     return ' '.join(str(ascii_lowercase.index(n.lower()) + 1) for n in text if n.isalpha())
+
+
+
+def alphabet_position(text):
+    alphabet = {  'a' : 1,
+                  'b' : 2,
+                  'c' : 3,
+                  'd' : 4,
+                  'e' : 5,
+                  'f' : 6,
+                  'g' : 7,
+                  'h' : 8,
+                  'i' : 9,
+                  'j' : 10,
+                  'k' : 11,
+                  'l' : 12,
+                  'm' : 13,
+                  'n' : 14,
+                  'o' : 15,
+                  'p' : 16,
+                  'q' : 17,
+                  'r' : 18,
+                  's' : 19,
+                  't' : 20,
+                  'u' : 21,
+                  'v' : 22,
+                  'w' : 23,
+                  'x' : 24,
+                  'y' : 25,
+                  'z' : 26, }
+    inds = []
+    for x in text.lower():
+        if x in alphabet:
+            inds.append(alphabet[x])
+    return ' '.join(([str(x) for x in inds]))
