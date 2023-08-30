70b76d16d25a18efaa4c92e36a64497e87ea6c18
vypiemzalyubov <dima.rza90@mail.ru>
2023-08-30 11:18:24 +0300

    Add 8 kyu task

---
+++ Codewars/8 kyu/convert_boolean_values_to_strings_yes_or_no.py
@@ -0,0 +1,7 @@
+# 8 kyu
+# Convert boolean values to strings 'Yes' or 'No'.
+# 
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word(boolean):
+    return 'Yes' if boolean else 'No'
