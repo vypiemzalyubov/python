75b793e0d05e51dc487b1f236a9a2308d0a70319
vypiemzalyubov <dima.rza90@mail.ru>
2023-08-30 18:27:39 +0300

    Add 8 kyu task

---
+++ Codewars/8 kyu/convert_a_string_to_an_array.py
@@ -0,0 +1,11 @@
+# 8 kyu
+# Convert a string to an array
+# 
+# Write a function to split a string and convert it into an array of words.
+# 
+# Examples (Input ==> Output):
+# "Robin Singh" ==> ["Robin", "Singh"]
+# "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+def string_to_array(s):
+    return s.split(' ')
