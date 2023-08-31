e9badfdf9a22f08edd1ad8b3ada18bfe311830a1
vypiemzalyubov <dima.rza90@mail.ru>
2023-08-31 12:52:44 +0300

    Add 7 kyu task

---
+++ Codewars/7 kyu/filter_the_number.py
@@ -0,0 +1,21 @@
+# 7 kyu
+# Filter the number
+# 
+# Filter the number
+# Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+# 
+# Task
+# Your task is to return a number from a string.
+# 
+# Details
+# You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+def filter_string(string):
+    return int(''.join(i for i in string if i.isdigit()))
+
+
+
+# Best Practices
+
+def filter_string(string):
+    return int(filter(str.isdigit, string))
