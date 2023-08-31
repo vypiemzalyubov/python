52d3a51f66f2dba8a10237e1ec834ac2fa9a4e64
vypiemzalyubov <dima.rza90@mail.ru>
2023-08-31 15:41:44 +0300

    Update 6 kyu task

--- Codewars/6 kyu/create_phone_number.py
+++ Codewars/6 kyu/create_phone_number.py
@@ -20,5 +20,5 @@ def create_phone_number(n):
 	return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
 
 def create_phone_number(n):
-    n = ''.join(map(str,n))
-    return '(%s) %s-%s'%(n[:3], n[3:6], n[6:])
+    n = ''.join(map(str, n))
+    return '(%s) %s-%s' % (n[:3], n[3:6], n[6:])
