b44deb11cbd23f9e660ae2f828780fd80141fca7
vypiemzalyubov <dima.rza90@mail.ru>
2023-09-01 12:16:19 +0300

    Add 6 kyu task

---
+++ Codewars/6 kyu/count_characters_in_your_string.py
@@ -0,0 +1,8 @@
+# 6 kyu
+# Count characters in your string
+# 
+# The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+# What if the string is empty? Then the result should be empty object literal, {}.
+
+def count(s):
+    return {c: s.count(c) for c in s}
