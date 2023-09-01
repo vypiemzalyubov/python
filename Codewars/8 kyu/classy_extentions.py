b99513671d8e57f8e52a996c7b5b6391cab45991
vypiemzalyubov <dima.rza90@mail.ru>
2023-09-01 12:42:25 +0300

    Add 8 kyu task

---
+++ Codewars/8 kyu/classy_extentions.py
@@ -0,0 +1,14 @@
+# 8 kyu
+# Classy Extentions
+# 
+# Classy Extensions, this kata is mainly aimed at the new JS ES6 Update introducing extends keyword. You will be preloaded with the Animal class, so you should only edit the Cat class.
+# Task
+# Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+# The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
+
+from preloaded import Animal
+
+class Cat(Animal):
+    
+    def speak(self):
+        return f'{self.name} meows.'
