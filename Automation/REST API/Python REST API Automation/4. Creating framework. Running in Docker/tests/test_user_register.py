bf9cd2c822b251675e01d6982baae8db62bebaca
vypiemzalyubov <dima.rza90@mail.ru>
2023-09-05 20:28:17 +0300

    Update test user register

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_register.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_register.py
@@ -9,20 +9,27 @@ from lib.my_requests import MyRequests
 class TestUserRegister(BaseCase):
 
     def test_create_user_successfully(self):
-
+        """Создание нового пользователя"""
         data = self.prepare_registration_data()
-
         response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
-
+        """Создание пользователя с существующим емэйлом"""
         email = "vinkotov@example.com"
         data = self.prepare_registration_data(email)
-
         response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_invalid_email(self):
+        """Создание пользователя с некорректным email - без символа @"""
+        email = "testexample.com"
+        data = self.prepare_registration_data(email)
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Invalid email format", f"Unexpected email {email}. Missing @"
