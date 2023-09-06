39883de09fefc15908060a285dc9f8fba3bc95e2
vypiemzalyubov <dima.rza90@mail.ru>
2023-09-06 22:50:28 +0300

    Update test_user_get.py

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_get.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_get.py
@@ -1,15 +1,16 @@
 import pytest
-# import allure
+import allure
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
 
 
-# @allure.epic("User getting cases")
+@allure.epic("User info getting cases")
 class TestUserGet(BaseCase):
 
-    def test_get_user_details_not_auth(self):
 
+    def test_get_user_details_not_auth(self):
+        """Получение данных пользователя без авторизации"""
         response = MyRequests.get("/user/2")
         
         Assertions.assert_json_has_key(response, "username")
@@ -18,14 +19,12 @@ class TestUserGet(BaseCase):
         Assertions.assert_json_has_not_key(response, "lastName")
 
     def test_get_user_details_auth_as_same_user(self):
-
+        """Получение данных авторизованного пользователя"""
         data = {
             "email": "vinkotov@example.com",
             "password": "1234"
         }        
-
         response1 = MyRequests.post("/user/login", data=data)
-
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
         user_id_from_auth_method = self.get_json_value(response1, "user_id")
@@ -34,7 +33,6 @@ class TestUserGet(BaseCase):
                                  headers={"x-csrf-token": token},
                                  cookies={"auth_sid": auth_sid}
                                  )
-        
         expected_fields = ["username", "email", "firstName", "lastName"]
         
         Assertions.assert_json_has_keys(response2, expected_fields)
