8ea1edce2edf0fe7a00190bc7c2ee4ef03d73396
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2023-09-06 15:36:29 +0300

    Update test user register

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_register.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_register.py
@@ -8,6 +8,7 @@ from lib.my_requests import MyRequests
 # @allure.epic("Registrations cases")
 class TestUserRegister(BaseCase):
 
+
     def test_create_user_successfully(self):
         """Создание нового пользователя"""
         data = self.prepare_registration_data()
@@ -15,6 +16,7 @@ class TestUserRegister(BaseCase):
 
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
+
 
     def test_create_user_with_existing_email(self):
         """Создание пользователя с существующим емэйлом"""
@@ -25,6 +27,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
 
+
     def test_create_user_with_invalid_email(self):
         """Создание пользователя с некорректным email - без символа @"""
         email = "testexample.com"
@@ -34,43 +37,24 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Invalid email format", f"Unexpected email {email}. Missing @"
 
-    # user_data = [
-    #     ("no_cookie"),
-    #     ("no_token")
-    # ]   
 
-    # @pytest.mark.parametrize("input, expected_results", user_data)
-    def test_create_user_without_specifying_one_of_fields(self):
+    @pytest.mark.parametrize("password, username, firstname, lastname, email", [
+        (None, "bot", "bot_fname", "bot_lname", "bot@example.com"),
+        ("1234", None, "bot_fname", "bot_lname", "bot@example.com"),
+        ("1234", "bot", None, "bot_lname", "bot@example.com"),
+        ("1234", "bot", "bot_fname", None, "bot@example.com"),
+        ("1234", "bot", "bot_fname", "bot_lname", None)
+    ])
+    def test_create_user_without_specifying_one_of_fields(self, password, username, firstname, lastname, email):
         """Создание пользователя без указания одного из полей"""
-        data1 =  {
-            "username": "learnqa",
-            "firstName": "learnqa",
-            "lastName": "learnqa",
-            "email": "test@example.com"
+        data = {
+            "password": password,
+            "username": username,
+            "firstName": firstname,
+            "lastName": lastname,
+            "email": email            
         }
-        data2 =  {
-            "password": "1234",
-            "firstName": "learnqa",
-            "lastName": "learnqa",
-            "email": "test@example.com"
-        }
-        data3 =  {
-            "password": "1234",
-            "username": "learnqa",
-            "lastName": "learnqa",
-            "email": "test@example.com"
-        }
-        data4 =  {
-            "password": "1234",
-            "username": "learnqa",
-            "firstName": "learnqa",
-            "email": "test@example.com"
-        }
-        data5 =  {
-            "password": "1234",
-            "username": "learnqa",
-            "firstName": "learnqa",
-            "lastName": "learnqa"
-        }        
-        response = MyRequests.post("/user/", data=data2)
-        print(response.content)
+        response = MyRequests.post("/user/", data=data)
+        
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {next(key for key, value in data.items() if value == None)}", f"Missing required param: {next(key for key, value in data.items() if value == None)}"
