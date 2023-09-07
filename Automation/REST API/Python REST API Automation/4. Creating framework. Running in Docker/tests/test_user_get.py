08be776d0f977ef6e411cadb07d306b7d5531388
vypiemzalyubov <dima.rza90@mail.ru>
2023-09-07 20:40:20 +0300

    Update tests

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_get.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_get.py
@@ -10,13 +10,15 @@ class TestUserGet(BaseCase):
 
 
     def test_get_user_details_not_auth(self):
-        """Получение данных пользователя без авторизации"""
+        """Получение данных пользователя без авторизации. 
+           Получиаем только username"""
         response = MyRequests.get("/user/2")
         
         Assertions.assert_json_has_key(response, "username")
         Assertions.assert_json_has_not_key(response, "email")
         Assertions.assert_json_has_not_key(response, "firstName")
         Assertions.assert_json_has_not_key(response, "lastName")
+
 
     def test_get_user_details_auth_as_same_user(self):
         """Получение данных авторизованного пользователя"""
