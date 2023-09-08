1ab54d497e44b63a1ded79a44b3410f1be9fd9e0
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2023-09-08 16:05:09 +0300

    Update test user edit

--- Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_edit.py
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_edit.py
@@ -93,52 +93,71 @@ class TestUserEdit(BaseCase):
                                  cookies={"auth_sid": auth_sid}
                                  )
         
-        Assertions.assert_json_value_by_name(response4, "lastName", last_name, "Wrong last name of the user after edit")
+        Assertions.assert_json_value_by_name(response4, "lastName", last_name, "Last name was changed, but shouldn't have been")
 
 
-    # @allure.description("Редактирование данных созданного пользователя, будучи авторизованным другим пользователем")
-    # def test_edit_new_created_user_by_another_authorized_user(self):
+    @allure.description("Редактирование данных созданного пользователя, будучи авторизованным другим пользователем")
+    def test_edit_new_created_user_by_another_authorized_user(self):
         
-    #     # REGISTER FIRST USER
-    #     register_data_first_user = self.prepare_registration_data()
-    #     response1 = MyRequests.post("/user/", data=register_data_first_user)
+        # REGISTER FIRST USER
+        email1 = f"{''.join(__import__('random').choice(__import__('string').ascii_letters) for _ in range(9))}@example.com"
+        register_data1 = self.prepare_registration_data(email=email1)
+        response1 = MyRequests.post("/user/", data=register_data1)
 
-    #     Assertions.assert_code_status(response1, 200)
-    #     Assertions.assert_json_has_key(response1, "id")
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
 
-    #     email = register_data_first_user["email"]
-    #     first_name_first_user = register_data_first_user["firstName"]
-    #     password_first_user = register_data_first_user["password"]
-    #     user_id_first_user = self.get_json_value(response1, "id")
-
-    #     # REGISTER SECOND USER
-    #     email_second_user = "4_user@test.com"
-    #     register_data_second_user = self.prepare_registration_data(email=email_second_user)
-    #     response2 = MyRequests.post("/user/", data=register_data_second_user)
-
-    #     Assertions.assert_code_status(response1, 200)
-    #     Assertions.assert_json_has_key(response1, "id")
-
-    #     password_second_user = register_data_second_user["password"]           
-    #     user_id_second_user = self.get_json_value(response1, "id")
-
-    #     # LOGIN SECOND USER
-    #     login_data_second_user = {
-    #         "email": email,
-    #         "password": password_first_user
-    #     }
-    #     response3 = MyRequests.post("/user/login", data=login_data_second_user)
+        first_name1 = register_data1["firstName"]
+        password1 = register_data1["password"]
+        user_id1 = self.get_json_value(response1, "id")
         
-    #     auth_sid_second_user = self.get_cookie(response3, "auth_sid")
-    #     token_second_user = self.get_header(response3, "x-csrf-token")
+        # REGISTER SECOND USER
+        email2 = f"{''.join(__import__('random').choice(__import__('string').ascii_letters) for _ in range(10))}@example.com"
+        register_data2 = self.prepare_registration_data(email=email2)
+        response2 = MyRequests.post("/user/", data=register_data2)
 
-    #     # EDIT FIRST USER
-    #     new_name = "Changed name"
-    #     response4 = MyRequests.put(f"/user/2432", 
-    #                              headers={"x-csrf-token": token_second_user},
-    #                              cookies={"auth_sid": auth_sid_second_user},
-    #                              data={"firstName": new_name}
-    #                              )
+        Assertions.assert_code_status(response2, 200)
+        Assertions.assert_json_has_key(response2, "id")
+
+        password2 = register_data2["password"]
+
+        # LOGIN SECOND USER
+        login_data1 = {
+            "email": email2,
+            "password": password2
+        }
+        response_login1 = MyRequests.post("/user/login", data=login_data1)
+        
+        auth_sid1 = self.get_cookie(response_login1, "auth_sid")
+        token1 = self.get_header(response_login1, "x-csrf-token")
+
+        # EDIT FIRST USER BY AUTHORIZED SECOND USER
+        new_name = "Test name"
+        response_edit = MyRequests.put(f"/user/{user_id1}", 
+                                 headers={"x-csrf-token": token1},
+                                 cookies={"auth_sid": auth_sid1},
+                                 data={"firstName": new_name}
+                                 )
+        
+        Assertions.assert_code_status(response_edit, 200)
+
+        # LOGIN FIRST USER
+        login_data2 = {
+            "email": email1,
+            "password": password1
+        }
+        response_login2 = MyRequests.post("/user/login", data=login_data2)
+        
+        auth_sid2 = self.get_cookie(response_login2, "auth_sid")
+        token2 = self.get_header(response_login2, "x-csrf-token")
+
+        # GET & CHECK FIRST USER
+        response_check = MyRequests.get(f"/user/{user_id1}", 
+                                 headers={"x-csrf-token": token2},
+                                 cookies={"auth_sid": auth_sid2}
+                                 )
+
+        Assertions.assert_json_value_by_name(response_check, "firstName", first_name1, "First name was changed, but shouldn't have been")
 
 
     @allure.description("Редактирование емэйла созданного пользователя на невалидный - без символа @, после авторизации тем же пользователем")
