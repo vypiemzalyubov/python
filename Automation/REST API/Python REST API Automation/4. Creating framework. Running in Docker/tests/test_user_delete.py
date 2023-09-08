7448b01fcaa4fc2d2ec891d0fe9850ab5726e2db
vypiemzalyubov <dima.rza90@mail.ru>
2023-09-08 22:26:49 +0300

    Add test_user_delete.py

---
+++ Automation/REST API/Python REST API Automation/4. Creating framework. Running in Docker/tests/test_user_delete.py
@@ -0,0 +1,10 @@
+import pytest
+import allure
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from lib.my_requests import MyRequests
+
+
+@allure.epic("User deleting cases")
+class TestUserEdit(BaseCase):
+    pass
