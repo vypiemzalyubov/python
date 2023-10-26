dda02cb02d4feb4ae72101bea88b5e02647b1d5f
vypiemzalyubov <dima.rza90@mail.ru>
2023-10-26 22:30:03 +0300

    Add solution

--- Stepik/Python basics and applications/1. Basic principles of the Python language/basic_principles_of_the_python_language.py
+++ Stepik/Python basics and applications/1. Basic principles of the Python language/basic_principles_of_the_python_language.py
@@ -13,3 +13,12 @@ print(sum([int(input()) for _ in range(int(input()))]))
 
 objects = input()
 print(len({id(x) for x in objects}))
+
+# Напишите реализацию функции closest_mod_5, принимающую в качестве единственного аргумента целое число x и возвращающую самое маленькое целое число y, такое что:
+# - y больше или равно x
+# - y делится нацело на 5
+
+def closest_mod_5(x):
+    while x % 5:
+        x += 1
+    return x
