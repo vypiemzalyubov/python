f9b235303d4d9f48bbc69e13cbd2a0f10be806a2
vypiemzalyubov <dima.rza90@mail.ru>
2023-11-16 20:04:06 +0300

    Update tasks in module 3

--- Stepik/Python Basics and application/3. Using Python - analyzing text/using_python_analyzing_text.py
+++ Stepik/Python Basics and application/3. Using Python - analyzing text/using_python_analyzing_text.py
@@ -54,3 +54,54 @@ for line in sys.stdin:
     line = line.rstrip()
     if re.search(r'\bcat\b', line):
         print(line)
+
+# Вам дана последовательность строк.
+# Выведите строки, содержащие две буквы "z", между которыми ровно три символа.
+
+for line in __import__('sys').stdin:
+    line = line.rstrip()
+    if __import__('re').search(r'z.{3}z', line):
+        print(line)
+
+# Вам дана последовательность строк.
+# Выведите строки, содержащие обратный слеш "\".
+
+for line in __import__('sys').stdin:
+    line = line.rstrip()
+    if __import__('re').search(r'\\', line):
+        print(line)
+
+# Вам дана последовательность строк.
+# Выведите строки, содержащие слово, состоящее из двух одинаковых частей (тандемный повтор).
+
+for line in __import__('sys').stdin:
+    line = line.rstrip()
+    if __import__('re').search(r'\b(\w+)\1\b', line):
+        print(line)
+
+# Вам дана последовательность строк.
+# В каждой строке замените все вхождения подстроки "human" на подстроку "computer"﻿ и выведите полученные строки.
+
+for line in __import__('sys').stdin:
+    line = line.rstrip()
+    print(__import__('re').sub(r'human', 'computer', line))
+
+# Вам дана последовательность строк.
+# В каждой строке замените первое вхождение слова, состоящего только из латинских букв "a" (регистр не важен), на слово "argh".
+
+for line in __import__('sys').stdin:
+    print(__import__('re').sub(r'\b[a]+\b', 'argh', line.rstrip(), 1, __import__('re').I))
+
+# Вам дана последовательность строк.
+# В каждой строке поменяйте местами две первых буквы в каждом слове, состоящем хотя бы из двух букв.
+# Буквой считается символ из группы \w.
+
+for line in __import__('sys').stdin:
+    print(__import__('re').sub(r'\b(\w)(\w)(\w*)\b', r'\2\1\3', line.rstrip()))
+
+# Вам дана последовательность строк.
+# В каждой строке замените все вхождения нескольких одинаковых букв на одну букву.
+# Буквой считается символ из группы \w.
+
+for line in __import__('sys').stdin:
+    print(__import__('re').sub(r'(\w)\1+', r'\1', line.rstrip()))
