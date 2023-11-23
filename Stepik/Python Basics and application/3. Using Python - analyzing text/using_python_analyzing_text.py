a010f87d6e352ad9cb7d9752d066c43142912ff0
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2023-11-23 07:26:54 +0300

    Finished module 3

--- Stepik/Python Basics and application/3. Using Python - analyzing text/using_python_analyzing_text.py
+++ Stepik/Python Basics and application/3. Using Python - analyzing text/using_python_analyzing_text.py
@@ -240,3 +240,35 @@ with open("dataset_24476_4.txt") as file:
 
 for k, v in sorted(artists.items(), key=lambda x: (x[1], x[0])):
     print(k)
+
+# Вам дано описание пирамиды из кубиков в формате XML.
+# Кубики могут быть трех цветов: красный (red), зеленый (green) и синий (blue﻿).
+# Для каждого кубика известны его цвет, и известны кубики, расположенные прямо под ним.
+
+# Пример:
+# <cube color="blue">
+#   <cube color="red">
+#     <cube color="green">
+#     </cube>
+#   </cube>
+#   <cube color="red">
+#   </cube>
+# </cube>
+ 
+# Введем понятие ценности для кубиков. Самый верхний кубик, соответствующий корню XML документа имеет ценность 
+# 1. Кубики, расположенные прямо под ним, имеют ценность 2. Кубики, расположенные прямо под нижележащими кубиками, имеют ценность 3. И т. д.
+# Ценность цвета равна сумме ценностей всех кубиков этого цвета.
+# Выведите через пробел три числа: ценности красного, зеленого и синего цветов.
+
+import xml.etree.ElementTree as ET
+
+tree = ET.fromstring(input())
+ans = {"red": 0, "green": 0, "blue": 0}
+
+def dfs(cube, res, depth):
+    res[cube.attrib["color"]] += depth
+    for i in cube.findall("cube"):
+        dfs(i, res, depth + 1)
+
+dfs(tree, ans, 1)
+print(ans["red"], ans["green"], ans["blue"])
