3979834e635a713f1af5d9afabf757ebb11d650e
dnikolaev <dnikolaev@tradingview.com>
2024-04-22 10:37:35 +0300

    Add new tasks

--- Stepik/Python Generation a course for advanced learners/05. Sets/sets.py
+++ Stepik/Python Generation a course for advanced learners/05. Sets/sets.py
@@ -299,3 +299,45 @@ print(*sorted(s3.difference(s1, s2), reverse=True))
 
 s1, s2, s3 = (set(int(i) for i in input().split()) for _ in range(3))
 print(*sorted(set(range(11)) - s1.union(s2, s3)))
+
+# 115. Используя генератор множеств, дополните приведенный код так, чтобы получить множество, содержащее уникальные значения списка items. 
+#      Результат вывести на одной строке, в упорядоченном виде, разделяя элементы одним символом пробела.
+# 
+#      Примечание 1. Обратите внимание, некоторые элементы списка – числа, а некоторые – строки, при этом строки необходимо трактовать как числа.
+#      Примечание 2. Чтобы вывести элементы множества в упорядоченном виде используйте следующий код: print(*sorted(myset))
+
+items = [10, '30', 30, 10, '56', 34, '12', 90, 89, 34, 45, '67', 12, 10, 90, 23, '45', 56, '56', 1, 5, '6', 5]
+print(*sorted({i if isinstance(i, int) else int(i) for i in items}))
+
+# 116. Используя генератор множеств, дополните приведенный код так, чтобы получить множество, содержащее первую букву каждого слова (в нижнем регистре) списка words. 
+#      Результат вывести на одной строке в алфавитном порядке, разделяя элементы одним символом пробела.
+
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+print(*sorted({i[0].lower() for i in words}))
+
+# 117. Используя генератор множеств, дополните приведенный код так, чтобы получить множество, содержащее уникальные слова (в нижнем регистре) строки sentence. 
+#      Результат вывести на одной строке в алфавитном порядке, разделяя элементы одним символом пробела.
+# 
+#      Примечание. Учтите, что знаки пунктуации не относятся к словам.
+
+sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+print(*sorted({i.strip(':,.!?();').lower() for i in sentence.split()}))
+
+# 118. Используя генератор множеств, дополните приведенный код так, чтобы получить множество, содержащее уникальные слова  строки sentence длиною меньше 4 символов. 
+#      Результат вывести на одной строке (в нижнем регистре) в алфавитном порядке, разделяя элементы одним символом пробела.
+# 
+#      Примечание. Учтите, что знаки пунктуации не относятся к словам.
+
+sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+print(*sorted({i.strip(':,.!?();').lower() for i in sentence.split() if len(i.strip(':,.!?();')) < 4}))
+
+# 119. Используя генератор множеств, дополните приведенный код так, чтобы он выбрал из списка files уникальные имена файлов c расширением .png, 
+#      независимо от регистра имен и расширений. Имена файлов вывести вместе с расширением, все на одной строке, в нижнем регистре, в алфавитном порядке через пробел.
+# 
+#      Примечание. Если бы список files содержал следующие имена файлов:
+#      files = ['python.png', 'qwerty.py', 'Python.PNg', 'apple.pnG', 'zebra.PNG',  'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git', 'ZeBrA.PnG']
+#      то ответом был бы:
+#      apple.png python.png zebra.png
+
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+print(*sorted({i.lower() for i in files if '.png' in i.lower()}))
