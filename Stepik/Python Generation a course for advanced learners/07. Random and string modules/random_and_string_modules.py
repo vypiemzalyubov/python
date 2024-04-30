1c94916fce7cb2cf9e33f42d9990c8ad0f9e8aa8
vypiemzalyubov <dima.rza90@mail.ru>
2024-04-30 21:07:20 +0300

    Add new tasks

--- Stepik/Python Generation a course for advanced learners/07. Random and string modules/random_and_string_modules.py
+++ Stepik/Python Generation a course for advanced learners/07. Random and string modules/random_and_string_modules.py
@@ -57,3 +57,32 @@ while len(ticket) != 7:
     ticket.add(__import__('random').randint(1, 49))
 
 print(*sorted(ticket), sep=' ')
+
+# 177. IP адрес состоит из четырех чисел из диапазона от 0 до 255 (включительно) разделенных точкой.
+#      Напишите функцию generate_ip(), которая с помощью модуля random  генерирует и возвращает случайный корректный IP адрес.
+# 
+#      Примечание 1. Пример правильного (неправильного) IP адреса:
+#      192.168.5.250        # правильный
+#      199.300.521.255      # неправильный
+#      Примечание 2. Вызывать функцию generate_ip() не нужно, требуется только реализовать.
+
+def generate_ip():
+    return '.'.join([str(__import__('random').randint(0, 255)) for _ in range(4)])
+
+# 178. Почтовый индекс в Латверии имеет вид: 
+#      LetterLetterNumber_NumberLetterLetter
+#      где Letter – заглавная буква английского алфавита, Number – число от 0 до 99 (включительно).
+#      Напишите функцию generate_index(), которая с помощью модуля random генерирует и возвращает случайный корректный почтовый индекс Латверии.
+# 
+#      Примечание 1. Пример правильного (неправильного) индекса Латверии:
+#      AB23_56VG          # правильный
+#      V3F_231GT          # неправильный
+#      Примечание 2. Обратите внимание на символ _ в почтовом индексе.
+#      Примечание 3. Вызывать функцию generate_index() не нужно, требуется только реализовать.
+
+from random import choice, randint
+from string import ascii_uppercase
+
+
+def generate_index():
+    return f'{choice(ascii_uppercase)}{choice(ascii_uppercase)}{randint(0, 99)}_{randint(0, 99)}{choice(ascii_uppercase)}{choice(ascii_uppercase)}'
